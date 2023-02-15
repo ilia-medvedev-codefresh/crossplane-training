9ef8facafd75bed2dec4d5d205880091cbfac191
Ilia Medvedev <ilia.medvedev@codefresh.io>
2023-02-15 10:19:42 +0200

    init env

---
+++ README.md
@@ -0,0 +1,5 @@
+Tasks:
+
+1. Create a simple Helm release with Crossplane that deploys vcluster to a namespace on K3D cluster
+2. Wrap the same thing in a composition
+3. Try to deploy something into vcluster using the same composition.
---
+++ scripts/init-k3d-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MYDIR=$(dirname $0)
+k3d cluster delete crossplane
+k3d cluster create crossplane
+k3d kubeconfig get crossplane > /tmp/k3s-crossplane.config
+export KUBECONFIG=/tmp/k3s-crossplane.config
+helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
+helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.21.0
+kubectl apply -f $MDIR/../argo-apps
