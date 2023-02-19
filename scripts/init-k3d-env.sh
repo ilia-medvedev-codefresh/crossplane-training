154127112fc7ecf05dc812ffaa830af3b7ee6008
Ilia Medvedev <ilia.medvedev@codefresh.io>
2023-02-19 12:28:41 +0200

    fix sync

--- scripts/init-k3d-env.sh
+++ scripts/init-k3d-env.sh
@@ -6,4 +6,4 @@ k3d kubeconfig get crossplane > /tmp/k3s-crossplane.config
 export KUBECONFIG=/tmp/k3s-crossplane.config
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
 helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.21.0
-kubectl -n argocd apply -f $MDIR/../argo-apps
+kubectl -n argocd apply -f $MYDIR/../argo-apps
